ca9a5c84fd06f7b954172cbd640eeeddb0b7907c
spaghettimedic <dsteiner247@gmail.com>
2021-10-25 15:54:58 -0400

    completed random character functions and length function

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,46 +1,51 @@
 // Assignment code here
 
-const keys = {
-  lowerCase: "abcdefghijklmnopqrstuvwxyz",
-  upperCase: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
-  number: "0123456789",
-  symbol: "`~!@#$%^&*()-_=+[]{}|\\:;<>,./?"
-};
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
 
-var getLength = function () {
+const randomFunc = {
+  lower: randomLower,
+  upper: randomUpper,
+  number: randomNumber,
+  symbol: randomSymbol
+};
+
+// Generator functions
+function randomLower() {
+  const lowers = 'abcdefghijklmnopqrstuvwxyz'
+  return lowers[Math.floor(Math.random() * lowers.length)];
+};
+function randomUpper() {
+  const uppers = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+  return uppers[Math.floor(Math.random() * uppers.length)];
+};
+function randomNumber() {
+  const numbers = '0123456789'
+  return numbers[Math.floor(Math.random() * numbers.length)];
+};
+function randomSymbol() {
+  const symbols = '`~!@#$%^&*()-_=+[]{}|\\:;<>,./?'
+  return symbols[Math.floor(Math.random() * symbols.length)];
+};
+
+var getLength = function (length) {
   var length = "";
   while (length === "" || length === null) {
     length = prompt("How long would you like your password to be? It must be between 8 and 128 characters in length.");
-  };
+  }
   if (length < 8 || length > 128) {
     window.alert("Your password length must be between 8 and 128 characters. Please try again.");
     return getLength();
-  };
-  return length;
+  }
+  if (isNaN(length)) {
+    window.alert("Enter numbers only please.");
+    return getLength();
+  }
+  console.log("Your password will be " + length + " characters in length.");
+  return +length;
 };
-
-
-// Randomized functions
-const getKey = [
-
-  function randomLower() {
-    return keys.lowerCase[Math.floor(Math.random() * keys.lowerCase.length)];
-  },
-  function randomUpper() {
-    return keys.upperCase[Math.floor(Math.random() * keys.upperCase.length)];
-  },
-  function randomNumber() {
-    return keys.number[Math.floor(Math.random() * keys.number.length)];
-  },
-  function randomSymbol() {
-    return keys.symbol[Math.floor(Math.random() * keys.symbol.length)];
-  },
-  console.log(randomLower(), randomUpper(), randomNumber(), randomSymbol())
-];
 
 
 // Write password to the #password input
@@ -71,10 +76,16 @@ function writePassword() {
     randomSymbol();
     console.log(randomSymbol());
   };
-  };
 
-  function generatePassword() {
+  if (confirmLower, confirmUpper, confirmNumber, confirmSymbol === false ) {
+    window.alert("You did not choose any characters to include. Please try again.");
+    writePassword();
+  }
+  console.log(generatePassword());
+};
 
+  function generatePassword(lower, upper, number, symbol, length) {
+    let passwordText = '';
   };
   
   var password = generatePassword();
