2a61188d3b3515eab20e318e04b9770eaf02c9c0
spaghettimedic <dsteiner247@gmail.com>
2021-10-25 21:03:10 -0400

    final product after tutor session

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,92 +1,51 @@
+const lowers = 'abcdefghijklmnopqrstuvwxyz'
+const uppers = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+const numbers = '0123456789'
+const symbols = '`~!@#$%^&*()-_=+[]{}|\\:;<>,./?'
+
+function generatePassword() {
+  let passwordLength = prompt("How long would you like your password to be? It must be between 8 and 128 characters in length.");
+  passwordLength = Number(passwordLength);
+  // check if number is between 8 and 128
+  if (passwordLength < 8 || passwordLength > 128) {
+    alert("Your password length must be between 8 and 128 characters. Please try again.");
+    return generatePassword();    
+  }
+  // check if number was entered for password length
+  if (isNaN(passwordLength)) {
+    alert("Only numbers can be entered for password length. Please try again.");
+    return generatePassword();
+  }
+
+  const includeLowerCase = confirm("Would you like to include lower case letters?");
+  const includeUpperCase = confirm("Would you like to include upper case letters?");
+  const includeNumbers = confirm("Would you like to include numbers?");
+  const includeSymbols = confirm("Would you like to include symbols?");
+  // check if no characters selected
+  if (includeLowerCase ===false && includeUpperCase === false && includeNumbers === false && includeSymbols === false) {
+    alert("You did not choose any characters to include. Please try again.");
+    return generatePassword();
+  };
+
+  let possibleCharacters = "";
+  if (includeLowerCase) {possibleCharacters += lowers}
+  if (includeUpperCase) {possibleCharacters += uppers}
+  if (includeNumbers) {possibleCharacters += numbers}
+  if (includeSymbols) {possibleCharacters += symbols}
+
+  let password = ""
+  for (let i = 0; i < passwordLength; i++) {
+    const randomIndex = Math.floor(Math.random() * (possibleCharacters.length - 1))
+    password += possibleCharacters[randomIndex]
+  }
+  return password
+}
+
+function writePassword() {
+  const password = generatePassword();
+  document.querySelector("#password").textContent = password
+}
+
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
-
-
-// Generator functions
-function randomLower() {
-  const lowers = 'abcdefghijklmnopqrstuvwxyz'
-  return lowers[Math.floor(Math.random() * lowers.length)];
-};
-function randomUpper() {
-  const uppers = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-  return uppers[Math.floor(Math.random() * uppers.length)];
-};
-function randomNumber() {
-  const numbers = '0123456789'
-  return numbers[Math.floor(Math.random() * numbers.length)];
-};
-function randomSymbol() {
-  const symbols = '`~!@#$%^&*()-_=+[]{}|\\:;<>,./?'
-  return symbols[Math.floor(Math.random() * symbols.length)];
-};
-
-
-// Write password to the #password input
-function writePassword() {
-  var randFuncs = [];
-  var length = getLength();
-  
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-  var confirmLower = window.confirm("Would you like to include lower case letters?");
-  if (confirmLower) {
-    randFuncs.push(randomLower);
-  };
-
-  var confirmUpper = window.confirm("Would you like to include upper case letters?");
-  if (confirmUpper) {
-    randFuncs.push(randomUpper);
-  };
-
-  var confirmNumber = window.confirm("Would you like to include numbers?");
-  if (confirmNumber) {
-    randFuncs.push(randomNumber);
-  };
-  
-  var confirmSymbol = window.confirm("Would you like to include symbols?");
-  if (confirmSymbol) {
-    randFuncs.push(randomSymbol);
-  };
-
-  if (confirmLower, confirmUpper, confirmNumber, confirmSymbol === false ) {
-    window.alert("You did not choose any characters to include. Please try again.");
-    writePassword();
-  }
-  console.log(randFuncs);
-  return randFuncs;
-
-  function getLength() {
-    var passLength = "";
-    while (passLength === "" || passLength === null) {
-      passLength = prompt("How long would you like your password to be? It must be between 8 and 128 characters in length.");
-    }
-    if (passLength < 8 || passLength > 128) {
-      window.alert("Your password length must be between 8 and 128 characters. Please try again.");
-      return getLength();
-    }
-    if (isNaN(passLength)) {
-      window.alert("Enter numbers only please.");
-      return getLength();
-    }
-    console.log("Your password will be " + passLength + " characters in length.");
-    return +passLength;
-  };
-  
-  function generatePassword(length, randFuncs) {
-    let passwordValue = '';
-    for (let i = 0; i < length; i++) {
-      randFuncs = randFuncs[Math.floor(Math.random() * randFuncs.length)]();
-
-      // passwordValue = ;
-    }
-    console.log(randFuncs);
-    console.log(passwordValue);
-    return passwordValue;
-  };
-};
-
-// Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
